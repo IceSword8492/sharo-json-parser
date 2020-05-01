1c12d8ad177a3931ceac95f95ad51690192effc0
IceSword8492 <cleared.for.approach.runway23@gmail.com>
2020-05-01 14:01:21 +0900

    tab -> space x4

--- index.js
+++ index.js
@@ -43,140 +43,140 @@ const SyntaxKind = {
  * @enum {number}
  */
 const CharacterCodes = {
-	nullCharacter: 0,
-	maxAsciiCharacter: 0x7F,
+    nullCharacter: 0,
+    maxAsciiCharacter: 0x7F,
 
-	lineFeed: 0x0A,              // \n
-	carriageReturn: 0x0D,        // \r
-	lineSeparator: 0x2028,
-	paragraphSeparator: 0x2029,
+    lineFeed: 0x0A,              // \n
+    carriageReturn: 0x0D,        // \r
+    lineSeparator: 0x2028,
+    paragraphSeparator: 0x2029,
 
-	// REVIEW: do we need to support this?  The scanner doesn't, but our IText does.  This seems
-	// like an odd disparity?  (Or maybe it's completely fine for them to be different).
-	nextLine: 0x0085,
+    // REVIEW: do we need to support this?  The scanner doesn't, but our IText does.  This seems
+    // like an odd disparity?  (Or maybe it's completely fine for them to be different).
+    nextLine: 0x0085,
 
-	// Unicode 3.0 space characters
-	space: 0x0020,   // " "
-	nonBreakingSpace: 0x00A0,   //
-	enQuad: 0x2000,
-	emQuad: 0x2001,
-	enSpace: 0x2002,
-	emSpace: 0x2003,
-	threePerEmSpace: 0x2004,
-	fourPerEmSpace: 0x2005,
-	sixPerEmSpace: 0x2006,
-	figureSpace: 0x2007,
-	punctuationSpace: 0x2008,
-	thinSpace: 0x2009,
-	hairSpace: 0x200A,
-	zeroWidthSpace: 0x200B,
-	narrowNoBreakSpace: 0x202F,
-	ideographicSpace: 0x3000,
-	mathematicalSpace: 0x205F,
-	ogham: 0x1680,
+    // Unicode 3.0 space characters
+    space: 0x0020,   // " "
+    nonBreakingSpace: 0x00A0,   //
+    enQuad: 0x2000,
+    emQuad: 0x2001,
+    enSpace: 0x2002,
+    emSpace: 0x2003,
+    threePerEmSpace: 0x2004,
+    fourPerEmSpace: 0x2005,
+    sixPerEmSpace: 0x2006,
+    figureSpace: 0x2007,
+    punctuationSpace: 0x2008,
+    thinSpace: 0x2009,
+    hairSpace: 0x200A,
+    zeroWidthSpace: 0x200B,
+    narrowNoBreakSpace: 0x202F,
+    ideographicSpace: 0x3000,
+    mathematicalSpace: 0x205F,
+    ogham: 0x1680,
 
-	_: 0x5F,
-	$: 0x24,
+    _: 0x5F,
+    $: 0x24,
 
-	_0: 0x30,
-	_1: 0x31,
-	_2: 0x32,
-	_3: 0x33,
-	_4: 0x34,
-	_5: 0x35,
-	_6: 0x36,
-	_7: 0x37,
-	_8: 0x38,
-	_9: 0x39,
+    _0: 0x30,
+    _1: 0x31,
+    _2: 0x32,
+    _3: 0x33,
+    _4: 0x34,
+    _5: 0x35,
+    _6: 0x36,
+    _7: 0x37,
+    _8: 0x38,
+    _9: 0x39,
 
-	a: 0x61,
-	b: 0x62,
-	c: 0x63,
-	d: 0x64,
-	e: 0x65,
-	f: 0x66,
-	g: 0x67,
-	h: 0x68,
-	i: 0x69,
-	j: 0x6A,
-	k: 0x6B,
-	l: 0x6C,
-	m: 0x6D,
-	n: 0x6E,
-	o: 0x6F,
-	p: 0x70,
-	q: 0x71,
-	r: 0x72,
-	s: 0x73,
-	t: 0x74,
-	u: 0x75,
-	v: 0x76,
-	w: 0x77,
-	x: 0x78,
-	y: 0x79,
-	z: 0x7A,
+    a: 0x61,
+    b: 0x62,
+    c: 0x63,
+    d: 0x64,
+    e: 0x65,
+    f: 0x66,
+    g: 0x67,
+    h: 0x68,
+    i: 0x69,
+    j: 0x6A,
+    k: 0x6B,
+    l: 0x6C,
+    m: 0x6D,
+    n: 0x6E,
+    o: 0x6F,
+    p: 0x70,
+    q: 0x71,
+    r: 0x72,
+    s: 0x73,
+    t: 0x74,
+    u: 0x75,
+    v: 0x76,
+    w: 0x77,
+    x: 0x78,
+    y: 0x79,
+    z: 0x7A,
 
-	A: 0x41,
-	B: 0x42,
-	C: 0x43,
-	D: 0x44,
-	E: 0x45,
-	F: 0x46,
-	G: 0x47,
-	H: 0x48,
-	I: 0x49,
-	J: 0x4A,
-	K: 0x4B,
-	L: 0x4C,
-	M: 0x4D,
-	N: 0x4E,
-	O: 0x4F,
-	P: 0x50,
-	Q: 0x51,
-	R: 0x52,
-	S: 0x53,
-	T: 0x54,
-	U: 0x55,
-	V: 0x56,
-	W: 0x57,
-	X: 0x58,
-	Y: 0x59,
-	Z: 0x5A,
+    A: 0x41,
+    B: 0x42,
+    C: 0x43,
+    D: 0x44,
+    E: 0x45,
+    F: 0x46,
+    G: 0x47,
+    H: 0x48,
+    I: 0x49,
+    J: 0x4A,
+    K: 0x4B,
+    L: 0x4C,
+    M: 0x4D,
+    N: 0x4E,
+    O: 0x4F,
+    P: 0x50,
+    Q: 0x51,
+    R: 0x52,
+    S: 0x53,
+    T: 0x54,
+    U: 0x55,
+    V: 0x56,
+    W: 0x57,
+    X: 0x58,
+    Y: 0x59,
+    Z: 0x5A,
 
-	ampersand: 0x26,             // &
-	asterisk: 0x2A,              // *
-	at: 0x40,                    // @
-	backslash: 0x5C,             // \
-	bar: 0x7C,                   // |
-	caret: 0x5E,                 // ^
-	closeBrace: 0x7D,            // }
-	closeBracket: 0x5D,          // ]
-	closeParen: 0x29,            // )
-	colon: 0x3A,                 // :
-	comma: 0x2C,                 // ,
-	dot: 0x2E,                   // .
-	doubleQuote: 0x22,           // "
-	equals: 0x3D,                // =
-	exclamation: 0x21,           // !
-	greaterThan: 0x3E,           // >
-	lessThan: 0x3C,              // <
-	minus: 0x2D,                 // -
-	openBrace: 0x7B,             // {
-	openBracket: 0x5B,           // [
-	openParen: 0x28,             // (
-	percent: 0x25,               // %
-	plus: 0x2B,                  // +
-	question: 0x3F,              // ?
-	semicolon: 0x3B,             // ;
-	singleQuote: 0x27,           // '
-	slash: 0x2F,                 // /
-	tilde: 0x7E,                 // ~
+    ampersand: 0x26,             // &
+    asterisk: 0x2A,              // *
+    at: 0x40,                    // @
+    backslash: 0x5C,             // \
+    bar: 0x7C,                   // |
+    caret: 0x5E,                 // ^
+    closeBrace: 0x7D,            // }
+    closeBracket: 0x5D,          // ]
+    closeParen: 0x29,            // )
+    colon: 0x3A,                 // :
+    comma: 0x2C,                 // ,
+    dot: 0x2E,                   // .
+    doubleQuote: 0x22,           // "
+    equals: 0x3D,                // =
+    exclamation: 0x21,           // !
+    greaterThan: 0x3E,           // >
+    lessThan: 0x3C,              // <
+    minus: 0x2D,                 // -
+    openBrace: 0x7B,             // {
+    openBracket: 0x5B,           // [
+    openParen: 0x28,             // (
+    percent: 0x25,               // %
+    plus: 0x2B,                  // +
+    question: 0x3F,              // ?
+    semicolon: 0x3B,             // ;
+    singleQuote: 0x27,           // '
+    slash: 0x2F,                 // /
+    tilde: 0x7E,                 // ~
 
-	backspace: 0x08,             // \b
-	formFeed: 0x0C,              // \f
-	byteOrderMark: 0xFEFF,
-	tab: 0x09,                   // \t
-	verticalTab: 0x0B,           // \v
+    backspace: 0x08,             // \b
+    formFeed: 0x0C,              // \f
+    byteOrderMark: 0xFEFF,
+    tab: 0x09,                   // \t
+    verticalTab: 0x0B,           // \v
 };
 
 /**
@@ -237,22 +237,22 @@ class JSONScanner {
  * @enum {number}
  */
 const ParseErrorCode = {
-	InvalidSymbol: 1,
-	InvalidNumberFormat: 2,
-	PropertyNameExpected: 3,
-	ValueExpected: 4,
-	ColonExpected: 5,
-	CommaExpected: 6,
-	CloseBraceExpected: 7,
-	CloseBracketExpected: 8,
-	EndOfFileExpected: 9,
-	InvalidCommentToken: 10,
-	UnexpectedEndOfComment: 11,
-	UnexpectedEndOfString: 12,
-	UnexpectedEndOfNumber: 13,
-	InvalidUnicode: 14,
-	InvalidEscapeCharacter: 15,
-	InvalidCharacter: 16
+    InvalidSymbol: 1,
+    InvalidNumberFormat: 2,
+    PropertyNameExpected: 3,
+    ValueExpected: 4,
+    ColonExpected: 5,
+    CommaExpected: 6,
+    CloseBraceExpected: 7,
+    CloseBracketExpected: 8,
+    EndOfFileExpected: 9,
+    InvalidCommentToken: 10,
+    UnexpectedEndOfComment: 11,
+    UnexpectedEndOfString: 12,
+    UnexpectedEndOfNumber: 13,
+    InvalidUnicode: 14,
+    InvalidEscapeCharacter: 15,
+    InvalidCharacter: 16
 };
 
 /**
@@ -350,9 +350,9 @@ class Location {
     path;
 
     /**
-	 * Matches the locations path against a pattern consisting of strings (for properties) and numbers (for array indices).
-	 * '*' will match a single segment, of any property name or index.
-	 * '**' will match a sequece of segments or no segment, of any property name or index.
+     * Matches the locations path against a pattern consisting of strings (for properties) and numbers (for array indices).
+     * '*' will match a single segment, of any property name or index.
+     * '**' will match a sequece of segments or no segment, of any property name or index.
      * @param {JSONPath} patterns
      * @return {boolean}
      */
@@ -397,71 +397,71 @@ class ParseOptions {
  */
 class JSONVisitor {
     /**
-	 * Invoked when an open brace is encountered and an object is started. The offset and length represent the location of the open brace.
+     * Invoked when an open brace is encountered and an object is started. The offset and length represent the location of the open brace.
      * @param {number} offset
      * @param {number} length
      */
-	onObjectBegin(offset, length) {}
+    onObjectBegin(offset, length) {}
 
-	/**
-	 * Invoked when a property is encountered. The offset and length represent the location of the property name.
+    /**
+     * Invoked when a property is encountered. The offset and length represent the location of the property name.
      * @param {string} property
      * @param {number} offset
      * @param {number} length
      */
-	onObjectProperty(property, offset, length) {}
+    onObjectProperty(property, offset, length) {}
 
-	/**
-	 * Invoked when a closing brace is encountered and an object is completed. The offset and length represent the location of the closing brace.
+    /**
+     * Invoked when a closing brace is encountered and an object is completed. The offset and length represent the location of the closing brace.
      * @param {number} offset
      * @param {number} length
-	 */
-	onObjectEnd(offset, length) {}
+     */
+    onObjectEnd(offset, length) {}
 
-	/**
-	 * Invoked when an open bracket is encountered. The offset and length represent the location of the open bracket.
+    /**
+     * Invoked when an open bracket is encountered. The offset and length represent the location of the open bracket.
      * @param {number} offset
      * @param {number} length
-	 */
-	onArrayBegin(offset, length) {}
+     */
+    onArrayBegin(offset, length) {}
 
-	/**
-	 * Invoked when a closing bracket is encountered. The offset and length represent the location of the closing bracket.
+    /**
+     * Invoked when a closing bracket is encountered. The offset and length represent the location of the closing bracket.
      * @param {number} offset
      * @param {number} length
-	 */
-	onArrayEnd(offset, length) {}
+     */
+    onArrayEnd(offset, length) {}
 
-	/**
-	 * Invoked when a literal value is encountered. The offset and length represent the location of the literal value.
+    /**
+     * Invoked when a literal value is encountered. The offset and length represent the location of the literal value.
      * @param {any} value
      * @param {number} offset
      * @param {number} length
-	 */
-	onLiteralValue(value, offset, length) {}
+     */
+    onLiteralValue(value, offset, length) {}
 
-	/**
-	 * Invoked when a comma or colon separator is encountered. The offset and length represent the location of the separator.
+    /**
+     * Invoked when a comma or colon separator is encountered. The offset and length represent the location of the separator.
      * @param {string} character
      * @param {number} offset
      * @param {number} length
-	 */
-	onSeparator(character, offset, length) {}
+     */
+    onSeparator(character, offset, length) {}
 
-	/**
-	 * When cmments are allowed, invoked when a line or block comment is encountered. The offset and length represent the location of the comment.
+    /**
+     * When cmments are allowed, invoked when a line or block comment is encountered. The offset and length represent the location of the comment.
      * @param {number} offset
      * @param {number} length
-	 */
-	onComment(offset, length) {}
+     */
+    onComment(offset, length) {}
 
-	/**
-	 * Invoked on an error.
+    /**
+     * Invoked on an error.
      * @param {ParseErrorCode} error
      * @param {number} offset
      * @param {number} length
-	 */
-	onError(errorm, offset, length) {}
+     */
+    onError(errorm, offset, length) {}
 }
 
 /**
@@ -1706,21 +1706,21 @@ function visit(text, visitor, options = ParseOptions.DEFAULT) {
      */
     function parseProperty() {
         if (_scanner.getToken() !== SyntaxKind.StringLiteral) {
-			handleError(ParseErrorCode.PropertyNameExpected, [], [SyntaxKind.CloseBraceToken, SyntaxKind.CommaToken]);
-			return false;
-		}
-		parseString(false);
-		if (_scanner.getToken() === SyntaxKind.ColonToken) {
-			onSeparator(':');
-			scanNext(); // consume colon
+            handleError(ParseErrorCode.PropertyNameExpected, [], [SyntaxKind.CloseBraceToken, SyntaxKind.CommaToken]);
+            return false;
+        }
+        parseString(false);
+        if (_scanner.getToken() === SyntaxKind.ColonToken) {
+            onSeparator(':');
+            scanNext(); // consume colon
 
-			if (!parseValue()) {
-				handleError(ParseErrorCode.ValueExpected, [], [SyntaxKind.CloseBraceToken, SyntaxKind.CommaToken]);
-			}
-		} else {
-			handleError(ParseErrorCode.ColonExpected, [], [SyntaxKind.CloseBraceToken, SyntaxKind.CommaToken]);
-		}
-		return true;
+            if (!parseValue()) {
+                handleError(ParseErrorCode.ValueExpected, [], [SyntaxKind.CloseBraceToken, SyntaxKind.CommaToken]);
+            }
+        } else {
+            handleError(ParseErrorCode.ColonExpected, [], [SyntaxKind.CloseBraceToken, SyntaxKind.CommaToken]);
+        }
+        return true;
     }
 
     /**
@@ -1769,40 +1769,40 @@ function visit(text, visitor, options = ParseOptions.DEFAULT) {
      */
     function parseArray() {
         onArrayBegin();
-		scanNext(); // consume open bracket
+        scanNext(); // consume open bracket
 
-		let needsComma = false;
-		while (
+        let needsComma = false;
+        while (
             _scanner.getToken() !== SyntaxKind.CloseBracketToken
             && _scanner.getToken() !== SyntaxKind.EOF
         ) {
-			if (_scanner.getToken() === SyntaxKind.CommaToken) {
-				if (!needsComma) {
-					handleError(ParseErrorCode.ValueExpected, [], []);
-				}
-				onSeparator(',');
-				scanNext(); // consume comma
-				if (
+            if (_scanner.getToken() === SyntaxKind.CommaToken) {
+                if (!needsComma) {
+                    handleError(ParseErrorCode.ValueExpected, [], []);
+                }
+                onSeparator(',');
+                scanNext(); // consume comma
+                if (
                     _scanner.getToken() === SyntaxKind.CloseBracketToken
                     && allowTrailingComma
                 ) {
-					break;
-				}
-			} else if (needsComma) {
-				handleError(ParseErrorCode.CommaExpected, [], []);
-			}
-			if (!parseValue()) {
-				handleError(ParseErrorCode.ValueExpected, [], [SyntaxKind.CloseBracketToken, SyntaxKind.CommaToken]);
-			}
-			needsComma = true;
-		}
-		onArrayEnd();
-		if (_scanner.getToken() !== SyntaxKind.CloseBracketToken) {
-			handleError(ParseErrorCode.CloseBracketExpected, [SyntaxKind.CloseBracketToken], []);
-		} else {
-			scanNext(); // consume close bracket
-		}
-		return true;
+                    break;
+                }
+            } else if (needsComma) {
+                handleError(ParseErrorCode.CommaExpected, [], []);
+            }
+            if (!parseValue()) {
+                handleError(ParseErrorCode.ValueExpected, [], [SyntaxKind.CloseBracketToken, SyntaxKind.CommaToken]);
+            }
+            needsComma = true;
+        }
+        onArrayEnd();
+        if (_scanner.getToken() !== SyntaxKind.CloseBracketToken) {
+            handleError(ParseErrorCode.CloseBracketExpected, [SyntaxKind.CloseBracketToken], []);
+        } else {
+            scanNext(); // consume close bracket
+        }
+        return true;
     }
 
     /**
@@ -1818,7 +1818,7 @@ function visit(text, visitor, options = ParseOptions.DEFAULT) {
             return parseString(true);
         default:
             return parseLiteral();
-		}
+        }
     }
 
     scanNext();
@@ -1831,13 +1831,13 @@ function visit(text, visitor, options = ParseOptions.DEFAULT) {
         return false;
     }
     if (!parseValue()) {
-		handleError(ParseErrorCode.ValueExpected, [], []);
-		return false;
-	}
-	if (_scanner.getToken() !== SyntaxKind.EOF) {
-		handleError(ParseErrorCode.EndOfFileExpected, [], []);
-	}
-	return true;
+        handleError(ParseErrorCode.ValueExpected, [], []);
+        return false;
+    }
+    if (_scanner.getToken() !== SyntaxKind.EOF) {
+        handleError(ParseErrorCode.EndOfFileExpected, [], []);
+    }
+    return true;
 }
 
 /**
@@ -1857,7 +1857,7 @@ function stripComments(text, replaceCh) {
     /**
      * @type {SyntaxKind}
      */
-	let kind;
+    let kind;
     let offset = 0;
     /**
      * @type {number}
@@ -1866,8 +1866,8 @@ function stripComments(text, replaceCh) {
 
     do {
         pos = _scanner.getPosition();
-		kind = _scanner.scan();
-		switch (kind) {
+        kind = _scanner.scan();
+        switch (kind) {
         case SyntaxKind.LineCommentTrivia:
         case SyntaxKind.BlockCommentTrivia:
         case SyntaxKind.EOF:
@@ -1879,7 +1879,7 @@ function stripComments(text, replaceCh) {
             }
             offset = _scanner.getPosition();
             break;
-		}
+        }
     } while (kind !== SyntaxKind.EOF);
     return parts.join('');
 }
@@ -1902,7 +1902,7 @@ function getNodeType(value) {
         return 'object';
     }
     default: return 'null';
-	}
+    }
 }
 
 (module || {}).exports = {
